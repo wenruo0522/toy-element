948f160f8e0d2404a235bdd762a8fbf2b7f38269
wenruo0522 <376567610@qq.com>
2024-07-18 01:46:41 +0800

    feat: init the simple button

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
+// @ts-ignore
 import App from './App.vue'
+import ToyElement from 'toy-element'
 
-createApp(App).mount('#app')
+createApp(App).use(ToyElement).mount('#app')
