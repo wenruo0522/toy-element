48fe76f60878c61d6f0c5ebeb7fac1caaa4554c0
wenruo0522 <376567610@qq.com>
2024-07-29 03:17:42 +0800

    fix(global): renew the deploy config yaml

--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -1,5 +1,5 @@
-import type {App, Plugin} from 'vue'
-import {each} from 'lodash-es'
+import type { App, Plugin } from 'vue'
+import { each } from 'lodash-es'
 
 type SFCWithInstall<T> = T & Plugin
 
